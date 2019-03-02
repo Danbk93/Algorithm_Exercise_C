8fbd6aaa3dc702ea751312c47e4c012f183a8c1e
BeomKeun Kim <tiger8229@gmail.com>
2019-03-03 07:10:35 +0900

    go to slleeeep33

--- 1697_hide_and_seak.cpp
+++ 1697_hide_and_seak.cpp
@@ -48,5 +48,4 @@ int main(void)
 		}
 
 	}
-	return 0;
 }
---
+++ 6588_goldbach.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+
+int main(void)
+{
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	
+
+
+}
