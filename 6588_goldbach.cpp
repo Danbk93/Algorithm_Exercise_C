c1dacce25293531e49df35a30ba8dc87e19667c1
Danbk <tiger8229@gmail.com>
2019-03-04 23:22:53 +0900

    ddd

--- 6588_goldbach.cpp
+++ 6588_goldbach.cpp
@@ -11,8 +11,35 @@ int main(void)
 	freopen("input.txt", "r", stdin);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	int num = 1;
+	int arr[1000001] = { 0 };
 
+	while (num != 0) {
+		cin >> num;
+		if (num == 0) {
+			break;
+		
+		}
+		for (int i = 1; i <= num; i++) {
+
+			arr[i] == i;
+
+		}
+
+		for (int i = 2; i <= num; i++) {
+			for (int j = 1; (i ^ 2)*j <=num;j++ ) {
+
+				arr[(i ^ 2)*j] = 0;
+			}
+			
+		
+		}
+
+		for (int i = 1; i <= num; i++) {
+
+			cout<<arr[i];
+
+		}
 	
-
-
+	}
 }
