988732b866b03a1c6531962a941dd671062bf75f
BeomKeun Kim <tiger8229@gmail.com>
2019-03-05 19:18:35 +0900

    6588

--- 6588_goldbach.cpp
+++ 6588_goldbach.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
-#include <queue>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -12,7 +12,38 @@ int main(void)
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int num = 1;
-	int arr[1000001] = { 0 };
+
+	int arr[1000001];
+	vector<int> v;
+	std::fill_n(arr, 1000001, 1);
+
+	for (int i = 2; i <= sqrt(1000000); i++) {
+		int sqr = pow(i, 2);
+
+		for (int j = 0; sqr + (i*j) <= 1000000; j++) {
+
+			arr[sqr + (i*j)] = 0;
+
+		}
+
+
+	}
+
+	//for (int i = 1; i <= 1000000; i++) {
+
+	//	cout << arr[i] << endl;
+
+	//}
+	//cout << endl;
+
+	for (int i = 3; i <= 1000000; i++) {
+
+		if (arr[i] == 1) {
+			v.push_back(i);
+
+		}
+
+	}
 
 	while (num != 0) {
 		cin >> num;
@@ -20,26 +51,22 @@ int main(void)
 			break;
 		
 		}
-		for (int i = 1; i <= num; i++) {
+		
 
-			arr[i] == i;
+		for (int i = 0; i < v.size(); i++) {
+			if (arr[num - v[i]] == 1)
+			{
 
-		}
+				cout << num << " = " << v[i] << " + " << num - v[i] << "\n";
 
-		for (int i = 2; i <= num; i++) {
-			for (int j = 1; (i ^ 2)*j <=num;j++ ) {
+				break;
 
-				arr[(i ^ 2)*j] = 0;
 			}
-			
 		
 		}
 
-		for (int i = 1; i <= num; i++) {
 
-			cout<<arr[i];
 
-		}
 	
 	}
 }
