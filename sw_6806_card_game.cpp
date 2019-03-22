59cf957451c565590b5f26876fa4957d088c4607
Danbk <tiger8229@gmail.com>
2019-03-22 22:26:14 +0900

    sw2

---
+++ sw_6806_card_game.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+
+int T;
+int whos[19];
+int gyu[9];
+int in[9];
+
+
+int main(void) {
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> T;
+
+	while (T--) {
+
+		for (int i=1; i <= 18; i++) {
+			int a = 0;
+			cin >> a;
+			if(a==i)
+		}
+
+		
+
+
+	}
+
+
+
+}
+
+
+
