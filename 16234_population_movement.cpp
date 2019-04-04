1fa1adf9b741156ea1733d63442eb9226becec6f
BeomKeun Kim <tiger8229@gmail.com>
2019-04-05 01:23:53 +0900

    d

--- 16234_population_movement.cpp
+++ 16234_population_movement.cpp
@@ -5,6 +5,31 @@
 
 using namespace std;
 int N, L, R;
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+int visited[51][51] = { 0, };
+
+void bfs(int a,int b) {	//check 배열은 방문 여부를 확인하는 배열
+	queue<pair<int, int>> q;
+	q.push(make_pair(a,b)); 
+	visited[a][b] = true;
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+			if (visited[nx][ny] != 0) continue;
+			if (check[y] == false) {
+				q.push(y); check[y] = true;
+			}
+		}
+	}
+}
+
 int main(void) {
 	freopen("input.txt", "r", stdin);
 	ios_base::sync_with_stdio(false);
@@ -12,7 +37,26 @@ int main(void) {
 
 	cin >> N >> L >> R;
 
+	int arr[51][51] = { 0, };
 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int k = 0;
+			cin >> arr[i][j];
+
+		}
+
+	}
+
+
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << arr[i][j];
+
+		}
+		cout << endl;
+	}
 
 }
 
