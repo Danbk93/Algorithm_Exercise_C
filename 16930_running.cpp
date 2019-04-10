cf8dc9eb8c00126cc7c46981abd937b435a21fa6
Danbk <tiger8229@gmail.com>
2019-04-10 20:38:41 +0900

    16930 1000일때 고치기

--- 16930_running.cpp
+++ 16930_running.cpp
@@ -12,7 +12,8 @@ int x_1, y_1, x_2, y_2;
 int dx[4] = { -1,0,1,0 };
 int dy[4] = { 0,1,0,-1 };
 int visited[1001][1001];
-
+int dist[1001][1001];
+int flag;
 void bfs(int a, int b) {
 	queue<pair<int,int>> q;
 	q.push(make_pair(a, b));
@@ -23,31 +24,75 @@ void bfs(int a, int b) {
 		int y = q.front().second;
 
 		q.pop();
-		int temp = 0;
-		while (temp < K) {
-			for (int i = 0; i < 4; i++) {
-				int nx = dx[i] + x;
-				int ny = dy[i] + y;
 
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] == '#') continue;
-				if (nx == x_2 && ny == y_2) {
+		/*for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				cout << visited[i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;*/
+		
+		if (x == x_2 && y == y_2) {
+			cout << dist[x][y];
+			break;
+		}
 
+
+		/*	for (int i = 0; i < 4; i++) {
+					int nx = dx[i]+ x;
+					int ny = dy[i]+ y;
+
+					if (nx < 1 || nx > N || ny < 1 || ny > M || map[nx][ny] == '#') continue;
+					if (visited[nx][ny] == 0) {
+						q.push(make_pair(nx, ny));
+
+						visited[nx][ny] = 1;
+
+						dist[nx][ny] = dist[x][y] + 1;
+					}
 				}
+
+			}*/
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = K; j >= 1; j--) {
+
+				flag = 0;
+				for (int n = j; n >= 1; n--) {
+					int nnx = dx[i] * n + x;
+					int nny = dy[i] * n + y;
+
+					if (nnx < 1 || nnx > N || nny < 1 || nny > M) continue;
+					if (map[nnx][nny] == '#') {
+						flag = 1;
+						break;
+					}
+				}
+				if (flag == 1) {
+					continue;
+				}
+				int nx = dx[i] * j + x;
+				int ny = dy[i] * j + y;
+
+				if (nx < 1 || nx > N || ny < 1 || ny > M ) continue;
 				if (visited[nx][ny] == 0) {
 					q.push(make_pair(nx, ny));
-					visited[nx][ny] == 1;
+					for (int z = j; z >= 1; z--) {
+						int nnx = dx[i] * z + x;
+						int nny = dy[i] * z + y;
+
+						visited[nnx][nny] = 1;
+
+					}
+					dist[nx][ny] = dist[x][y] + 1;
 				}
-
 			}
-			temp++;
+
+
+
 		}
-	
-
 	}
-
-
-	
-
 }
 
 int main(void) {
@@ -58,14 +103,14 @@ int main(void) {
 	cin >> N >> M >> K;
 
 	//input
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
 			cin >> map[i][j];
 		}
 	}
 
-	//for (int i = 0; i < N; i++) {
-	//	for (int j = 0; j < M; j++) {
+	//for (int i = 1; i <= N; i++) {
+	//	for (int j = 1; j <= M; j++) {
 	//		cout<< map[i][j];
 	//	}
 	//	cout << endl;
@@ -75,7 +120,9 @@ int main(void) {
 
 	bfs(x_1, y_1);
 
-
+	if (visited[x_2][y_2] == 0) {
+		cout << -1;
+	}
 
 }
 
