1a4dcb02d688758f527ef8e96d6ef94cb006493c
BeomKeun Kim <tiger8229@gmail.com>
2019-08-29 18:34:07 +0900

    2007년

---
+++ 1924_2007년.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int N;
+
+int main() {
+
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+	for (int i = 1; i < 10; i++) {
+
+		cout << N << " * " << i << " = " << N * i << '\n';
+
+
+	}
+
+}
---
+++ 2741_N찍기.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int N;
+
+int main() {
+
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		
+		cout << i+1<<'\n';
+
+
+	}
+
+}
