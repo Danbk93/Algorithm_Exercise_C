af151a8f7e4e8b145a79ffc22a89a3380e552d48
Danbk <tiger8229@gmail.com>
2019-09-04 18:10:42 +0900

    뒤집기 푸는중

---
+++ kakao_15999_뒤집기.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<math.h>
+
+using namespace std;
+int n, m;
+char arr[2001][2001];
+int tmp[2001][2001];
+int main() {
+
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+
+		}
+
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (j + 1 < m && arr[i][j] != arr[i][j + 1]) {
+				tmp[i][j] = 1;
+				tmp[i][j + 1] = 1;
+			}
+			if (i + 1 < n && arr[i][j] != arr[i + 1][j]) {
+				tmp[i][j] = 1;
+				tmp[i + 1][j] = 1;
+			}
+		}
+	}
+
+
+
+	long long diff = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (tmp[i][j] == 1) {
+				diff++;
+			}
+		}
+
+	}
+
+	cout << (long long)pow(2, n*m - diff) % 1000000007;
+
+}
